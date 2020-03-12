d1e764034651388c0499828b7ba3b9d2b1c64ae9
code-art7 <ls62235@gmail.com>
2020-03-12 20:36:52 +0530

    git issue solve

--- cargo_management_system/cargoms/backend.py
+++
@@ -1,8 +0,0 @@
-"""from .models import admin_login
-from django.contrib.auth.models import User
-
-class LoginBackend:
-    def authenticate(self, username=None, password=None, model=None):
-        if model == "Student":
-            lookup_model = admin_login
-        return user"""
