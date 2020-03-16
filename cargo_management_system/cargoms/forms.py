1a7ef029c9cb09530b1d24dcfba4777d86453a72
code-art7 <ls62235@gmail.com>
2020-03-16 22:03:41 +0530

    Guru's Register and login 2

--- cargo_management_system/cargoms/forms.py
+++ cargo_management_system/cargoms/forms.py
@@ -1,28 +1,53 @@
 from django import forms
 from .models import admin_login
-from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-
-class login(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-    class Meta:
-        model = admin_login
-        fields = ['log_id','name','password']
+from django.core.exceptions import ValidationError
 
 class SignUpForm(UserCreationForm):
-    full_name = forms.CharField(max_length=100)
-    username = forms.CharField(max_length=100)
-    email = forms.EmailField(max_length=150)
-    permissionId = forms.CharField(max_length=6)
+    full_name = forms.CharField(label='Full Name', max_length=100 , required=True)
+    username = forms.CharField(label='login ID', min_length=6, max_length=10, required=True)
+    email = forms.EmailField(label='E-mail', max_length=150, required=True)
+    permissionId = forms.CharField( label='Permission ID', max_length=6, required=True)
+
+    def clean_username(self):
+        username = self.cleaned_data['username'].lower()
+        r = User.objects.filter(username=username)
+        if r.count():
+            raise  ValidationError("Username already exists")
+        return username
+ 
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        r = User.objects.filter(email=email)
+        if r.count():
+            raise  ValidationError("Email already exists")
+        return email
+ 
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+ 
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Password don't match")
+ 
+        return password2
+ 
+    def save(self, commit=True):
+        user = User.objects.create_user(
+            self.cleaned_data['username'],
+            self.cleaned_data['email'],
+            self.cleaned_data['password1']
+        )
+        return user
 
     class Meta:
         model = User
         fields = ('full_name', 'username',  'email', 'permissionId')
 
 class SignInForm(AuthenticationForm):
-    username = forms.CharField(max_length=100)
-    Name = forms.CharField(max_length=100)
+    username = forms.CharField( label='Login ID', max_length=100)
 
     class meta:
         model : User 
-        feilds = ('name', 'username', 'password')              
+        feilds = ('username', 'password')          
--- cargo_management_system/cargoms/views.py
+++ cargo_management_system/cargoms/views.py
@@ -1,15 +1,39 @@
 from django.shortcuts import render, HttpResponse, redirect
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate,login
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from .forms import SignUpForm, SignInForm
+from django.contrib import auth, messages
 
-def login(request):
-     form = SignInForm()
-     return render(request, 'login.html', { 'signinform' : form })
+def login_v(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+ 
+        else:
+            messages.error(request, 'Error wrong username/password')
+    form = SignInForm()
+    return render(request, 'login.html', { 'signinform' : form })
 
 def registerUser(request):
-     form = SignUpForm()
-     return render(request, 'registerUser.html', { 'registrationform': form })
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+
+            messages.error(request, 'User Created Successfully')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'registerUser.html', {'registrationform': form})
 
 def home(request):
      return render(request, 'home.html')
